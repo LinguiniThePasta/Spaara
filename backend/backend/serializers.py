40d0ac0b45be076f6a79139e4bd6a7bfa87c64d4
LinguiniThePasta <lfh20130915@gmail.com>
2024-09-26 09:59:37 -0400

    Finished Login with JWT and Registration

--- backend/backend/serializers.py
+++ backend/backend/serializers.py
@@ -21,6 +21,8 @@ class RegisterSerializer(serializers.ModelSerializer):
         # Check if password is okay
         if not re.match("^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]{8,}$", password):
             errorDict['password'] = 'Password must have at least 8 characters, 1 uppercase character, 1 lowercase character, 1 number, and 1 special character.'
+        if password == email:
+            errorDict['password'] += 'Password cannot be the same as email.'
         if not validate_email(email):
             errorDict['email'] = "email is invalid."
         if errorDict:
@@ -52,3 +54,38 @@ class LoginSerializer(serializers.Serializer):
             raise serializers.ValidationError(errorDict)
         return data
 
+
+class UpdateInfoSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = User
+        fields = ['username', 'email', 'password']
+    def validate(self, data):
+        username = data.get('username')
+        email = data.get('email')
+        password = data.get('password')
+        errorDict = collections.defaultdict(str)
+        # Check if the username contains special characters
+        if not re.match("^[a-zA-Z0-9_]*$", username):
+            errorDict['username'] = 'Username can only contain alphanumeric characters.'
+        # Check if password is okay
+        if not re.match("^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]{8,}$", password):
+            errorDict['password'] = 'Password must have at least 8 characters, 1 uppercase character, 1 lowercase character, 1 number, and 1 special character.'
+        if password == email:
+            errorDict['password'] += 'Password cannot be the same as email.'
+        if not validate_email(email):
+            errorDict['email'] = "email is invalid."
+        if errorDict:
+            raise serializers.ValidationError(errorDict)
+        return data
+    def update(self, instance, validated_data):
+        instance_username = validated_data.get('username', instance.username)
+        instance_email = validated_data.get('email', instance.email)
+        instance_password = validated_data.get('password', instance.password)
+        password = validated_data.get('password', None)
+
+        if password:
+            instance.set_password(password)
+
+        # Save the updated user instance
+        instance.save()
+        return instance
