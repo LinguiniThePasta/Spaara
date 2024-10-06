a6962dec6035051ffddb23995ac86d33def96ada
LinguiniThePasta <lfh20130915@gmail.com>
2024-10-06 16:09:47 -0400

    Prototyped a TodoList, this one doesn't work super well yet but it's a proof of concept Contains ability to add checkmarks Contains ability to clear an item Contains ability to change quantity

--- frontend/app/_layout.tsx
+++ frontend/app/_layout.tsx
@@ -1,14 +1,14 @@
 import { Stack } from "expo-router";
 //import { NavigationContainer } from "@react-navigation/native";
 //import { createNativeStackNavigator } from "@react-navigation/native-stack";
-
+/*
 export default function RootLayout() {
   return (
     <Stack>
       <Stack.Screen name="index" />
     </Stack>
   );
-  /*return (
+  return (
     <NavigationContainer>
       <Stack.Navigator>
         <Stack.Screen
@@ -23,5 +23,34 @@ export default function RootLayout() {
         />
       </Stack.Navigator>
     </NavigationContainer>
-  );*/
+  );
 }
+*/
+
+
+
+// Import gesture handler root view
+import {GestureHandlerRootView} from 'react-native-gesture-handler';
+import {StatusBar, View, StyleSheet, Text} from 'react-native';
+import {ThemeProvider, theme} from "react-native-design-system";
+import React from "react";
+import "react-native-get-random-values";
+import {TodoList} from "@/app/TodoList";
+
+export default function RootLayout({children}) {
+    return (
+        <GestureHandlerRootView style={styles.container}>
+            <ThemeProvider theme={theme}>
+                <TodoList/>
+            </ThemeProvider>
+        </GestureHandlerRootView>
+    );
+}
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        backgroundColor: "white",
+    },
+});
+
+
