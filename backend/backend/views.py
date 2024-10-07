d6a4c8c37d06de793b4c7feccb6f6c9efcb17607
Aidan Velleca <45907878+AidanV0908@users.noreply.github.com>
2024-10-06 20:53:57 -0400

    Update views.py
    Accidentally committed part of another branch

--- backend/backend/views.py
+++ backend/backend/views.py
@@ -6,12 +6,12 @@ from rest_framework.response import Response
 from rest_framework import status
 from rest_framework_simplejwt.tokens import RefreshToken
 from . import serializers
-from .models import User, ShoppingList, Recipe, FavoriteItem
+from .models import User, ShoppingList, Recipe
 from rest_framework.permissions import IsAuthenticated
 from django.contrib.auth.decorators import login_required
 import pandas as pd;
 
-from .serializers import SaveShoppingListSerializer, SaveRecipeSerializer, FavoriteItemSerializer
+from .serializers import SaveShoppingListSerializer, SaveRecipeSerializer
 
 
 class RegisterView(APIView):
@@ -113,36 +113,7 @@ class RecipeView(APIView):
         else:
             recipes = user.recipes.all()
             serializer = SaveRecipeSerializer(recipes, many=True)
-            return Response(serializer.data, status=status.HTTP_200_OK)\
-            
-class AddFavoriteView(APIView):
-    permission_classes = [IsAuthenticated]
-
-    def post(self, request):
-        serializer = FavoriteItemSerializer(data=request.data)
-        if serializer.is_valid():
-            # Associate the favorite item with the current user
-            serializer.save(user=request.user)
-            return Response(serializer.data, status=status.HTTP_201_CREATED)
-        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
-
-          
-class RemoveFavoriteView(APIView):
-    permission_classes = [IsAuthenticated]
-
-    def delete(self, request):
-        data = request.data()
-        favorite_id = data.get('favorite_id', None)
-        if (favorite_id):
-            try:
-                favorite_item = FavoriteItem.objects.get(id=favorite_id, user=request.user)
-                favorite_item.delete()
-                return Response(status=status.HTTP_204_NO_CONTENT)
-            except FavoriteItem.DoesNotExist:
-                return Response({'error': 'Favorite item not found'}, status=status.HTTP_404_NOT_FOUND)
-        else:
-            return Response({'message': 'No favorite id in request'}, status=status.HTTP_400_BAD_REQUEST)
-
+            return Response(serializer.data, status=status.HTTP_200_OK)
 
 class ExportShoppingListView(APIView):
     permission_classes = [IsAuthenticated]
