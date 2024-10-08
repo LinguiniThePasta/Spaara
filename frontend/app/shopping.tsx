437a45c920e2b31422e86ec67946de4ce6e46e77
LinguiniThePasta <lfh20130915@gmail.com>
2024-10-08 17:09:34 -0400

    Added the shopping list UI to the react native app - needs more work to make it look good.

--- frontend/app/shopping.tsx
+++ frontend/app/shopping.tsx
@@ -1,148 +1,85 @@
-import { StatusBar } from "expo-status-bar";
-import {StyleSheet, Text, View, Image, TextInput, SafeAreaView, KeyboardAvoidingView, Platform} from "react-native";
-import React from "react";
-import NavigationButton from '@/components/NavigationButton';
-import {CoreBridge, HistoryBridge, RichText, TaskListBridge, Toolbar, useEditorBridge} from '@10play/tentap-editor';
-
-const spaaraLogoImage = require('@/assets/images/SpaaraLogo.png');
-
-//const Stack = createNativeStackNavigator();
-//const router = useRouter();
-//router.navigate("/login");
-
-
-
-
-
-//const pushLogin = () => router.push("/login")
-//const pushLogin = () => alert("I wanna push!!!!");
+import React from 'react';
+import {
+    SafeAreaView,
+    View,
+    KeyboardAvoidingView,
+    Platform,
+    StyleSheet,
+    Text,
+} from 'react-native';
+import { CoreBridge, HistoryBridge, RichText, TaskListBridge, Toolbar, useEditorBridge } from '@10play/tentap-editor';
+import NavigationButton from "@/components/NavigationButton";
 
 export default function Shopping() {
-  //const pushLogin = () => router.push("/login")
-  const pushLogin = () => alert("Log in");
-  const [usernameText, onUsernameChangeText] = React.useState('');
-  const [passwordText, onPasswordChangeText] = React.useState('');
-  const editor = useEditorBridge({
+    const editor = useEditorBridge({
         bridgeExtensions: [
-            CoreBridge.extendExtension({ content: 'taskList+'}),
+            CoreBridge.extendExtension({ content: 'taskList+' }),
             TaskListBridge,
             HistoryBridge
         ],
         autofocus: true,
         avoidIosKeyboard: true,
+        initialContent,
     });
 
-  return (
-    <View style={shoppingListStyles.container}>
-
-        <View style={shoppingListStyles.headerContainer}>
-          <NavigationButton label="Back" type="back" destination="/welcome"/>
-          <Text style={shoppingListStyles.headerLabel}>Log in</Text>
-          <View style={shoppingListStyles.headerSpacer}/>
+    return (
+        <View style={loginStyles.container}>
+            <View style={loginStyles.headerContainer}>
+                <NavigationButton label="Back" type="back" destination="/welcome" />
+                <Text style={loginStyles.headerLabel}>Log in</Text>
+                <View style={loginStyles.headerSpacer} />
+            </View>
+            <SafeAreaView style={loginStyles.contentContainer}>
+                <RichText editor={editor} />
+                <KeyboardAvoidingView
+                    behavior={Platform.OS === 'ios' ? 'padding' : 'height'}
+                    style={loginStyles.keyboardAvoidingView}
+                >
+                    <Toolbar editor={editor} />
+                </KeyboardAvoidingView>
+            </SafeAreaView>
         </View>
+    );
+};
 
-        <SafeAreaView style={shoppingListStyles.contentContainer}>
-          <RichText editor={editor}/>
-          <KeyboardAvoidingView
-              behavior={Platform.OS === 'ios' ? 'padding' : 'height'}
-              style={shoppingListStyles.keyboardAvoidingView}
-          >
-              <Toolbar editor={editor}/>
-          </KeyboardAvoidingView>
-        </SafeAreaView>
-
-        <StatusBar style='auto'/>
-      </View>
-  );
-}
-
-
-
-const shoppingListStyles = StyleSheet.create({
-
-  container: {
-    flex: 1,
-    backgroundColor: '#FFFBEE',
-    alignItems: 'center',
-  },
-
-  headerContainer: {
-    flexDirection: 'row',
-    width: '100%',
-    height: 100,
-    backgroundColor: '#F6AA1C',
-    alignItems: 'center',
-    justifyContent: 'space-between',
-  },
-
-  headerLabel: {
-    width: 290,
-    marginTop: 50,
-    marginBottom: 10,
-    color: '#232528',
-    fontSize: 28,
-    textAlign: 'center',
-  },
-
-  headerSpacer: {
-    width: 50,
-    height: 50,
-    marginTop: 50,
-    marginBottom: 10,
-  },
-
-  contentContainer: {
-    flex: 1,
-    flexDirection: 'column',
-    alignItems: 'center',
-    marginTop: 30,
-  },
-
-  separatorContainer: {
-    flexDirection: 'row',
-    width: 325,
-    height: 25,
-    marginTop: 25,
-    marginBottom: 35,
-    alignItems: 'center',
-    justifyContent: 'space-between',
-  },
-
-  separatorLabel: {
-    width: 41,
-    height: 20,
-    color: '#959595',
-    fontSize: 15,
-    textAlign: 'center'
-  },
-
-  separatorSpacer: {
-    width: 142,
-    height: 1,
-    marginVertical: 12,
-    backgroundColor: '#959595',
-    borderRadius: 100,
-  },
-  
-  textInputField: {
-    height: 20,
-    width: 325,
-    margin: 25,
-    fontSize: 15,
-    borderBottomWidth: 2,
-    borderBottomColor: '#959595',
-    color: '#232528',
-  },
-
-  loginButtonsContainer: {
-    alignItems: 'center',
-    marginTop: 50,
-  },
-
-  keyboardAvoidingView: {
-        position: 'absolute',
+const loginStyles = StyleSheet.create({
+    container: {
+        flex: 1,
+        backgroundColor: '#FFFBEE',
+        alignItems: 'center',
+    },
+    headerContainer: {
+        flexDirection: 'row',
         width: '100%',
-        bottom: 0,
-  },
-
+        height: 100,
+        backgroundColor: '#F6AA1C',
+        alignItems: 'center',
+        justifyContent: 'space-between',
+    },
+    headerLabel: {
+        flex: 1,
+        marginTop: 50,
+        marginBottom: 10,
+        color: '#232528',
+        fontSize: 28,
+        textAlign: 'center',
+    },
+    headerSpacer: {
+        width: 50,
+        height: 50,
+    },
+    contentContainer: {
+        flex: 1,
+        width: '100%',
+        paddingHorizontal: 16,
+        justifyContent: 'center',
+    },
+    keyboardAvoidingView: {
+        width: '100%',
+        position: 'relative',
+        justifyContent: 'flex-end',
+        paddingBottom: 10,
+    },
 });
+
+const initialContent = `<p>This is a basic example!</p>`;
