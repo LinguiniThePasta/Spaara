7061f238baeb6c5617f1db6cf245439e28abd6c6
theNareynater <whitacre@purdue.edu>
2024-10-09 00:31:23 -0400

    Merge branch 'frontend-ShoppingListUI-Lingyu' into LogInFrontEnd-Nathan
    - fixed some more merging issues
    - fixed navigation bugs

---
+++ frontend/styles.ts
@@ -0,0 +1,31 @@
+import {StyleSheet} from "react-native";
+
+const styles = StyleSheet.create({
+
+  container: {
+    flex: 1,
+    backgroundColor: '#FFFBEE',
+    alignItems: 'center',
+  },
+
+  logoImageContainer: {
+    paddingVertical: 110,
+  },
+
+  logoImage: {
+    width: 300,
+    height: 84,
+  },
+
+  logInButtonsContainer: {
+    flexDirection: 'column',
+    alignItems: 'center',
+  },
+
+  logInButtonsRow: {
+    flexDirection: 'row',
+    alignItems: 'center',
+  },
+});
+
+export default styles;
