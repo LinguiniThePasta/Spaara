079898676e5bb54528d372bacca14cf7d27109b4
LinguiniThePasta <lfh20130915@gmail.com>
2024-10-17 16:32:48 -0400

    Added Favorited Item View

--- backend/backend/urls.py
+++ backend/backend/urls.py
@@ -26,17 +26,7 @@ urlpatterns = [
     path('api/user/login', LoginView.as_view(), name='login'),
     path('api/user/register', RegisterView.as_view(), name='register'),
     path('api/user/update_info', UpdateInfoView.as_view(), name='update_info'),
-    path('api/shopping/create', ShoppingListView.as_view(), name='save_shopping_list'),
-    path('api/shopping/get', ShoppingListView.as_view(), name='get_shopping_list'),
-    path('api/shopping/delete', ShoppingListView.as_view(), name='delete_shopping_list'),
-    path('api/shopping/export', ExportShoppingListView.as_view(), name='export_shopping_list'),
-    path('api/recipe/create', RecipeView.as_view(), name='save_recipe'),
-    path('api/recipe/get', RecipeView.as_view(), name='get_recipe'),
-    path('api/recipe/export', ExportRecipeView.as_view(), name='export_recipe'),
-    path('api/recipe/delete', RecipeView.as_view(), name='delete_recipe'),
-    path('api/token/refresh', TokenRefreshView.as_view(), name='token_refresh'),
-    path('api/favorites/get', FavoriteView.as_view(), name='get_favorite'),
-    path('api/favorites/add', FavoriteView.as_view(), name='add_favorite'),
-    path('api/favorites/delete', FavoriteView.as_view(), name='delete_favorite'),
+    path('api/user/favorite/toggle', FavoriteView.as_view(), name='favorite_toggle'),
+
 ]
 
