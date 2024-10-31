3a5d89a9bf3942794f0c8b438b4646cdbb797114
AidanV0908 <45907878+AidanV0908@users.noreply.github.com>
2024-10-31 16:07:45 -0400

    Fix no uuid

--- backend/backend/views.py
+++ backend/backend/views.py
@@ -16,7 +16,7 @@ from .utils import send_verification_email
 from django.utils.http import urlsafe_base64_decode
 from django.utils.encoding import force_str
 from django.contrib.auth.tokens import default_token_generator
-
+import uuid
 
 class RegisterView(APIView):
     def post(self, request):
@@ -36,7 +36,7 @@ class VerifyEmailView(APIView):
             user = User.objects.get(pk=uid)
         except (TypeError, ValueError, OverflowError, User.DoesNotExist):
             user = None
-            
+
         if user is not None and default_token_generator.check_token(user, token):
             user.is_active = True
             user.save()
