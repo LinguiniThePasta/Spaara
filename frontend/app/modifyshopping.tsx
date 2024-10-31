6fd67e465a27a09c5271c334de744b404e195220
Ian Clausen <iclausen7991@gmail.com>
2024-10-30 21:52:22 -0400

    it now changed to display the recpies why you press recipes

--- frontend/app/modifyshopping.tsx
+++ frontend/app/modifyshopping.tsx
@@ -22,7 +22,7 @@ import {globalStyles} from "@/styles/globalStyles";
 import Header from "@/components/Header";
 import {useDispatch, useSelector} from 'react-redux';
 import * as SecureStore from 'expo-secure-store';
-import {CheckItem} from '@/components/Item';
+import Recipe from './recipe';
 //import { setSearchQuery } from '../store/shoppingListSlice';
 
 
@@ -47,16 +47,20 @@ export default function ShoppingListScreen() {
     ]);
 
     const [recipeTemp, setRecipe] = useState([
-        {id: 1, name: 'Beefed Banana'},
-        {id: 2, name: 'Porked Lemons'},
+        {id: 1, title: 'Beefed Banana'},
+        {id: 2, title: 'Porked Lemons'},
     ]);
+
 
     const [modalVisible, setModalVisible] = useState(false);
     const [selectedButton, setSelectedButton] = useState('Favorite');
+    const [notSelectedButton, setNotSelectedButton] = useState(false);
+    const [contentVisable, setContentVisable] = useState('Favorite');
 
 
     const handlePress = (button) => {
         setSelectedButton(button);
+        setContentVisable(button);
     };
 
 
@@ -216,15 +220,22 @@ export default function ShoppingListScreen() {
         )
     };
 
-    const testRender = ({item}) => {
-        return (
-            <CheckItem item={item}></CheckItem>
-        );
-    };
-
     const renderFavoriteItem = ({item}) => (
         <View style={styles.itemContainer}>
             <Text style={styles.itemTitle}>{item.title}</Text>
+        </View>
+    );
+
+    const renderRecipe = ({item}) => (
+        <View style={styles.recipeContainer}>
+            <View style={styles.recipeLeft}>
+                <Text style={styles.itemTitle}>{item.title}</Text>
+            </View>
+            <View style={styles.recipeRight}>
+                <Pressable onPress={() => console.log('Add pressed for ${item.title}')}>
+                    <Icon name="add-outline" size={20} color={Colors.light.primaryText} style={styles.icon}/>                    
+                </Pressable>
+            </View>
         </View>
     );
 
@@ -232,16 +243,17 @@ export default function ShoppingListScreen() {
         <View style={styles.container}>
             <SafeAreaView style={styles.container}>
                 <View>
-                    <Header header={`${shoppingListName}`} backButton={true} backLink={"/shopping"} noProfile={true}></Header>
+                    <Header header={`${shoppingListName}`} backButton={true} backLink={"/shopping"} noProfile={false}></Header>
                     {/*<Text style={styles.itemTitle}>$10.00 Budget</Text>*/}
                 </View>
+
                 <FlatList
                     data={shoppingItems}
                     keyExtractor={(item) => item.id}
-                    renderItem={testRender}
+                    renderItem={renderItem}
                     contentContainerStyle={styles.listContainer}
-                    
                 />
+
                 <TouchableOpacity style={styles.heartButton} onPress={() => setModalVisible(true)}>
                     <Icon name="heart-outline" size={24} color={Colors.light.background}/>
                 </TouchableOpacity>
@@ -288,16 +300,30 @@ export default function ShoppingListScreen() {
                                 </Text>
                             </Pressable>
                         </View>
-                        {selectedButton === 'Favorite' && (
-                            <Text style={styles.favoriteHeaderText}>Add Favorites</Text>)}
-                        {selectedButton === 'Recipe' && (<Text style={styles.favoriteHeaderText}>Add Recipes</Text>)}
 
-                        <FlatList
+                        {selectedButton === 'Favorite' && (
+                            <Text style={styles.favoriteHeaderText}>Add Favorites</Text>
+                        )}
+                        {selectedButton === 'Recipe' && (
+                            <Text style={styles.favoriteHeaderText}>Add Recipes</Text>)
+                        }
+                        {contentVisable === 'Favorite' && (
+                            <FlatList
                             data={favoriteItems}
                             renderItem={renderFavoriteItem}
                             keyExtractor={item => item.id.toString()}
                             style={styles.flatList}
-                        />
+                            />
+                        )}
+                        {contentVisable === 'Recipe' && (
+                            <FlatList
+                                data={recipeTemp}
+                                renderItem={renderRecipe}
+                                keyExtractor={item => item.id.toString()}
+                                style={styles.flatList}
+                            />
+                        )}
+                        
                         {/* Remove text inputs */}
 
                     </View>
@@ -476,6 +502,24 @@ const styles = StyleSheet.create({
         marginLeft: 10,
     },
     recipeContainer: {
-
+        width: '100%',
+        alignItems: 'center',
+        flexDirection: 'row',
+        justifyContent: 'space-between',
     },
+    recipeLeft: {
+        alignSelf: 'flex-start',
+        alignItems: 'center',
+        flexDirection: 'row',
+    },
+    recipeRight: {
+        alignItems: 'center',
+        alignSelf: 'flex-end',
+        flexDirection: 'row',
+    },
+    plusButton: {
+        borderWidth: 2,
+        borderColor: Colors.light.secondaryText,
+    },
+
 });
