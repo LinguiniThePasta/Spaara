7b1199bb5cbb93a5667cdde909e1db0b64fd443e
AidanV0908 <45907878+AidanV0908@users.noreply.github.com>
2024-11-10 14:36:37 -0500

    UI button to get to addresses from map

--- frontend/components/UnifiedIcon.tsx
+++ frontend/components/UnifiedIcon.tsx
@@ -2,15 +2,17 @@
 import React from 'react';
 import Ionicons from 'react-native-vector-icons/Ionicons';
 import FontAwesome from 'react-native-vector-icons/FontAwesome';
+import Material from 'react-native-vector-icons/MaterialIcons';
 // Import other icon libraries as needed
 
-const UnifiedIcon = ({ type, name, size, style }) => {
+const UnifiedIcon = ({ type, name, size, style, color }) => {
   switch (type) {
     case 'ionicon':
-      return <Ionicons name={name} size={size} style={style}/>;
+      return <Ionicons name={name} size={size} style={style} color={color}/>;
     case 'fontawesome':
-      return <FontAwesome name={name} size={size} style={style} />;
-    // Add more cases here for other icon libraries
+      return <FontAwesome name={name} size={size} style={style} color={color}/>;
+      case 'materialicon':
+        return <Material name={name} size={size} style={style} color={color}/>;
     default:
       return null;
   }
